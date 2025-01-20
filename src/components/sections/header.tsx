3199c80a2fc87a49cc7a9f6018598e44da1cadab
Usama Nasar <usamanasar38@gmail.com>
2025-01-20 21:34:14 +0500

    feat: render name and descriptoin from constant

--- src/components/sections/header.tsx
+++ src/components/sections/header.tsx
@@ -1,3 +1,4 @@
+import { DATA } from "@/data/resume";
 import Link from "next/link";
 import NavLinks from "../nav-links";
 
@@ -7,13 +8,13 @@ const Header = () => {
     <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-[48%] lg:flex-col lg:justify-between lg:py-24">
       <div>
         <h1 className="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl">
-          <Link href="/">Usama Nasar</Link>
+          <Link href="/">{DATA.name}</Link>
         </h1>
         <h2 className="mt-3 text-lg font-medium tracking-tight text-slate-200 sm:text-xl">
-          Full Stack Engineer
+          {DATA.role}
         </h2>
         <p className="mt-4 max-w-xs leading-normal">
-          I build scalable, efficient, user-centric, pixel-perfect digital experiences for the web.
+          {DATA.description}
         </p>
         <nav className="nav hidden lg:block" aria-label="In-page jump links">
           <ul className="mt-16 w-max">
@@ -25,13 +26,13 @@ const Header = () => {
         <li className="mr-5 shrink-0 text-xs">
           <a
             className="block hover:text-slate-200"
-            href="https://github.com/usamanasar38"
+            href={DATA.contact.social.GitHub.url}
             target="_blank"
             rel="noreferrer noopener"
-            aria-label="GitHub (opens in a new tab)"
-            title="GitHub"
+            aria-label={`${DATA.contact.social.GitHub.name} (opens in a new tab)`}
+            title={DATA.contact.social.GitHub.name}
           >
-            <span className="sr-only">GitHub</span>
+            <span className="sr-only">{DATA.contact.social.GitHub.name}</span>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 16 16"
@@ -46,13 +47,13 @@ const Header = () => {
         <li className="mr-5 shrink-0 text-xs">
           <a
             className="block hover:text-slate-200"
-            href="https://www.linkedin.com/in/usama-nasar/"
+            href={DATA.contact.social.LinkedIn.url}
             target="_blank"
             rel="noreferrer noopener"
-            aria-label="LinkedIn (opens in a new tab)"
-            title="LinkedIn"
+            aria-label={`${DATA.contact.social.LinkedIn.name} (opens in a new tab)`}
+            title={DATA.contact.social.LinkedIn.name}
           >
-            <span className="sr-only">LinkedIn</span>
+            <span className="sr-only">{DATA.contact.social.LinkedIn.name}</span>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
