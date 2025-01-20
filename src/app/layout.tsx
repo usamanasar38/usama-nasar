619c2c731d3f5f9332b59db0eeda355b414cf3b5
Usama Nasar <usamanasar38@gmail.com>
2025-01-20 21:23:50 +0500

    feat: add smooth scrolling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth">
       <body
         className={`${inter.className} bg-slate-900 leading-relaxed text-slate-400 antialiased selection:bg-teal-300 selection:text-teal-900`}
       >
