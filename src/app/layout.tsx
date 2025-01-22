5226f23d3e1da61873a102d08d6ac5d91a7bc6de
Usama Nasar <usamanasar38@gmail.com>
2025-01-22 12:36:44 +0500

    style: add fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,53 @@
 import type { Metadata } from "next";
-import { Inter } from 'next/font/google';
+import localFont from 'next/font/local';
 import "./globals.css";
 
-const inter = Inter()
+const inter = localFont({
+  src: [
+    {
+      path: './fonts/inter/Inter-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './fonts/inter/Inter-Italic.woff2',
+      weight: '400',
+      style: 'italic',
+    },
+    {
+      path: './fonts/inter/Inter-Medium.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: './fonts/inter/Inter-MediumItalic.woff2',
+      weight: '500',
+      style: 'italic',
+    },
+    {
+      path: './fonts/inter/Inter-SemiBold.woff2',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: './fonts/inter/Inter-SemiBoldItalic.woff2',
+      weight: '600',
+      style: 'italic',
+    },
+    {
+      path: './fonts/inter/Inter-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: './fonts/inter/Inter-BoldItalic.woff2',
+      weight: '700',
+      style: 'italic',
+    },
+  ],
+  adjustFontFallback: 'Arial',
+  variable: '--font-inter',
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,7 +62,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <body
-        className={`${inter.className} bg-slate-900 leading-relaxed text-slate-400 antialiased selection:bg-teal-300 selection:text-teal-900`}
+        className={`${inter.variable} bg-slate-900 leading-relaxed text-slate-400 antialiased selection:bg-teal-300 selection:text-teal-900`}
       >
         {children}
       </body>
