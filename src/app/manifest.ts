c316c3c339386879e69e05acaa6f76dec3b8e7ee
Usama Nasar <usamanasar38@gmail.com>
2025-01-24 15:53:25 +0500

    chore: add manifest

---
+++ src/app/manifest.ts
@@ -0,0 +1,41 @@
+import { DATA, METADATA } from "@/data/resume";
+import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: DATA.name,
+    short_name: DATA.name,
+    description: METADATA.description as string,
+    start_url: "/",
+    display: "standalone",
+    background_color: "#0f172a",
+    theme_color: "#0f172a",
+    icons: [
+      {
+        src: "/favicon.ico",
+        sizes: "any",
+        type: "image/x-icon",
+      },
+      {
+        src: "/favicon-16x16.png",
+        sizes: "16x16",
+        type: "image/png",
+      },
+      {
+        src: "/favicon-32x32.png",
+        sizes: "32x32",
+        type: "image/png",
+      },
+      {
+        src: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  };
+}
